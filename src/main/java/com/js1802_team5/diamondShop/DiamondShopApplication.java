d8e48355cd7a373d99d1e370d6e7c5597bc38e30
HuTi0801 <buihuutien3@gmail.com>
2024-05-18 22:08:29 +0700

    first commit

---
+++ src/main/java/com/js1802_team5/diamondShop/DiamondShopApplication.java
@@ -0,0 +1,13 @@
+package com.js1802_team5.diamondShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamondShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiamondShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/js1802_team5/diamondShop/DiamondShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js1802_team5.diamondShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiamondShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
