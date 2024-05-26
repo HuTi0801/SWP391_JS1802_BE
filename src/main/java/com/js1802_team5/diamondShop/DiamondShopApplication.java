7f8608738c6aa0ecd6b726f4137c2eb3ef8805d5
HuTi0801 <buihuutien3@gmail.com>
2024-05-26 13:54:09 +0700

    SearchProduct+AddToCart

--- src/main/java/com/js1802_team5/diamondShop/DiamondShopApplication.java
+++ src/main/java/com/js1802_team5/diamondShop/DiamondShopApplication.java
@@ -14,8 +14,8 @@ import org.springframework.context.annotation.Bean;
 @RequiredArgsConstructor
 public class DiamondShopApplication {
 
-	private final DiamondRepo diamondRepo;
-	private final DiamondShellRepo diamondShellRepo;
+//	private final DiamondRepo diamondRepo;
+//	private final DiamondShellRepo diamondShellRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiamondShopApplication.class, args);
@@ -25,49 +25,49 @@ public class DiamondShopApplication {
 		return new CommandLineRunner() {
 			@Override
 			public void run(String... args) throws Exception {
-				Diamond diamond1 = Diamond.builder()
-						.cut("EX")
-						.color("F")
-						.caratWeight(3.6f)
-						.clarity("VS2")
-						.price(11045000d)
-						.origin("Natural diamond")
-						.quantity(100)
-						.certificateNumber("123456789")
-						.build();
-
-				Diamond diamond2 = Diamond.builder()
-						.cut("EX")
-						.color("E")
-						.caratWeight(3.6f)
-						.clarity("VS1")
-						.price(11421000d)
-						.origin("Artificial diamond")
-						.quantity(100)
-						.certificateNumber("987654321")
-						.build();
-
-				diamondRepo.save(diamond1);
-				diamondRepo.save(diamond2);
-
-				DiamondShell diamondShell_1 = DiamondShell.builder()
-						.secondaryStoneType("Diamond")
-						.material("18K")
-						.gender("Male")
-						.price(10000000)
-						.quantity(10)
-						.build();
-
-				DiamondShell diamondShell_2 = DiamondShell.builder()
-						.secondaryStoneType("Diamond")
-						.material("24K")
-						.gender("Female")
-						.price(25000000)
-						.quantity(25)
-						.build();
-
-				diamondShellRepo.save(diamondShell_1);
-				diamondShellRepo.save(diamondShell_2);
+//				Diamond diamond1 = Diamond.builder()
+//						.cut("EX")
+//						.color("F")
+//						.caratWeight(3.6f)
+//						.clarity("VS2")
+//						.price(11045000d)
+//						.origin("Natural diamond")
+//						.quantity(100)
+//						.certificateNumber("123456789")
+//						.build();
+//
+//				Diamond diamond2 = Diamond.builder()
+//						.cut("EX")
+//						.color("E")
+//						.caratWeight(3.6f)
+//						.clarity("VS1")
+//						.price(11421000d)
+//						.origin("Artificial diamond")
+//						.quantity(100)
+//						.certificateNumber("987654321")
+//						.build();
+//
+//				diamondRepo.save(diamond1);
+//				diamondRepo.save(diamond2);
+//
+//				DiamondShell diamondShell_1 = DiamondShell.builder()
+//						.secondaryStoneType("Diamond")
+//						.material("18K")
+//						.gender("Male")
+//						.price(10000000)
+//						.quantity(10)
+//						.build();
+//
+//				DiamondShell diamondShell_2 = DiamondShell.builder()
+//						.secondaryStoneType("Diamond")
+//						.material("24K")
+//						.gender("Female")
+//						.price(25000000)
+//						.quantity(25)
+//						.build();
+//
+//				diamondShellRepo.save(diamondShell_1);
+//				diamondShellRepo.save(diamondShell_2);
 			}
 		};
 	}
