a5d48f853f791cc2c58bfd58cd566b9bb43c8e19
PhucDuyen <phucduyenne1011@gmail.com>
2024-06-22 13:31:28 +0700

    Create VnPayService

--- src/main/java/com/js1802_team5/diamondShop/services/VnPayService.java
+++ src/main/java/com/js1802_team5/diamondShop/services/VnPayService.java
@@ -1,2 +1,11 @@
-package com.js1802_team5.diamondShop.services;public interface VnPayService {
+package com.js1802_team5.diamondShop.services;
+
+import com.js1802_team5.diamondShop.models.response_models.VnPayResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.UnsupportedEncodingException;
+
+public interface VnPayService {
+    VnPayResponse createPayment(HttpServletRequest request, Integer cusId, long amount, String bankCode, String language) throws UnsupportedEncodingException;
+    VnPayResponse vnPayReturn(HttpServletRequest request) throws UnsupportedEncodingException;
 }
