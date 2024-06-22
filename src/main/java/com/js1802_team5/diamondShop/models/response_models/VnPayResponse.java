9c9fb51c1ed57e6934ad9069c8990623b316c588
PhucDuyen <phucduyenne1011@gmail.com>
2024-06-22 13:30:30 +0700

    Create VnPayResponse

--- src/main/java/com/js1802_team5/diamondShop/models/response_models/VnPayResponse.java
+++ src/main/java/com/js1802_team5/diamondShop/models/response_models/VnPayResponse.java
@@ -1,2 +1,19 @@
-package com.js1802_team5.diamondShop.models.response_models;public class VnPayResponse {
+package com.js1802_team5.diamondShop.models.response_models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VnPayResponse {
+    private int status;
+    private String message;
+    private String url;
+    private Map<String, String> paymentDetails;
 }
