c780cbc0d6292ddc48348a416e96c720d0229eef
PhucDuyen <phucduyenne1011@gmail.com>
2024-06-26 15:45:43 +0700

    Create transaction repo

--- src/main/java/com/js1802_team5/diamondShop/repositories/TransactionRepo.java
+++ src/main/java/com/js1802_team5/diamondShop/repositories/TransactionRepo.java
@@ -1,2 +1,7 @@
-package com.js1802_team5.diamondShop.repositories;public interface TransactionRepo {
+package com.js1802_team5.diamondShop.repositories;
+
+import com.js1802_team5.diamondShop.models.entity_models.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepo extends JpaRepository<Transaction, Integer> {
 }
