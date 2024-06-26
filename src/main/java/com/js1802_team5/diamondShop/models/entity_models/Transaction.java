d383048ac494b64dda7c92e74baa53fdab693b17
PhucDuyen <phucduyenne1011@gmail.com>
2024-06-26 15:40:03 +0700

    Update function create vnPayController, remove bank code

---
+++ src/main/java/com/js1802_team5/diamondShop/models/entity_models/Transaction.java
@@ -0,0 +1,2 @@
+package com.js1802_team5.diamondShop.models.entity_models;public class Transaction {
+}
---
+++ src/main/java/com/js1802_team5/diamondShop/models/request_models/TransactionRequest.java
@@ -0,0 +1,2 @@
+package com.js1802_team5.diamondShop.models.request_models;public class TransactionRequest {
+}
