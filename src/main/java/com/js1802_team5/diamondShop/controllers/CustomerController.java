c5cb3cd65e7647f49bda226eec1ef1af3fdf6149
PhucDuyen <phucduyenne1011@gmail.com>
2024-07-06 22:40:19 +0700

    Create new CustomerController

--- src/main/java/com/js1802_team5/diamondShop/controllers/CustomerController.java
+++ src/main/java/com/js1802_team5/diamondShop/controllers/CustomerController.java
@@ -1,2 +1,19 @@
-package com.js1802_team5.diamondShop.controllers;public class CustomerController {
+package com.js1802_team5.diamondShop.controllers;
+
+import com.js1802_team5.diamondShop.models.request_models.CustomerRequest;
+import com.js1802_team5.diamondShop.models.response_models.Response;
+import com.js1802_team5.diamondShop.services.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth/customer")
+@RequiredArgsConstructor
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @PostMapping("/update-info-customer/{customerId}")
+    public Response updateCustomer(@PathVariable("customerId") int customerId, @RequestBody CustomerRequest customerRequest) {
+        return customerService.updateCustomer(customerId, customerRequest);
+    }
 }
