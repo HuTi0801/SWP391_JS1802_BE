db05c84ec42daa545902aa25a347fee2413bf61a
PhucDuyen <phucduyenne1011@gmail.com>
2024-07-06 22:44:43 +0700

    Create new function updateCustomer into CustomerService

--- src/main/java/com/js1802_team5/diamondShop/services/CustomerService.java
+++ src/main/java/com/js1802_team5/diamondShop/services/CustomerService.java
@@ -1,2 +1,8 @@
-package com.js1802_team5.diamondShop.services;public interface CustomerService {
+package com.js1802_team5.diamondShop.services;
+
+import com.js1802_team5.diamondShop.models.request_models.CustomerRequest;
+import com.js1802_team5.diamondShop.models.response_models.Response;
+
+public interface CustomerService {
+    Response updateCustomer(int customerId, CustomerRequest customerRequest);
 }
